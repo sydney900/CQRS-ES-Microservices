b4668d4877be443347ade1c70da6978fca69205a
Bing <sydney900@hotmail.com>
2018-05-07 23:23:17 +1000

    Add NodeJs kafka client

---
+++ NodejsClient/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var manager = require("./manager");
+
+manager.listBrokers();
+manager.listTopics();
+manager.getSingleTopic();
+manager.listTopicPartitions();
+manager.getSingleTopicPartition();
+
