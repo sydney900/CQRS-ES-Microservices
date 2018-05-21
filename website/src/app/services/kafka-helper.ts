6482484b76efbf7042781905dde967452389d262
Bing <sydney900@hotmail.com>
2018-05-21 23:35:06 +1000

    added support kafka node

--- website/src/app/services/kafka-helper.ts
+++ website/src/app/services/kafka-helper.ts
@@ -1,170 +1,134 @@
-///<reference path="../../../node_modules/@types/node/index.d.ts"/>
-import KafkaRest from "kafka-rest";
-import { Observable, Subject, bindCallback } from 'rxjs';
+import { Client, Producer, Consumer, Offset } from "kafka-node";
+import { Observable, Subject, bindCallback, from } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 
 export class KafkaSender {
-	constructor(
-		public apiUrl: string,
-		public topicName: string,
-		public partitionId: number = null
-	) { }
+  constructor(
+    public zookeeperConnectionString: string,
+    public topicName: string,
+    public partitionId: number = 0,
+    public compressionAttributes: number = 0 //0: No compression, 1: Compress using GZip, 2: Compress using snappy
+  ) {
+    this.ready = false;
+  }
 
-	private kafka: any;
-	private target: any;
+  private client: Client;
+  private producer: Producer;
+  private ready: boolean;
 
-	private tryToInitialize(): void {
-		if (!this.kafka) {
-			this.kafka = new KafkaRest({ 'url': this.apiUrl });
-			this.target = this.kafka.topic(this.topicName);
-			if (this.partitionId)
-				this.target = this.target.partition(this.partitionId);
-		}
-	}
+  private tryToInitialize(): void {
+    if (this.client) {
+      this.client = new Client(this.zookeeperConnectionString);
+      this.producer = new Producer(this.client, { requireAcks: 1 });
 
-	sendCommand(command): Observable<any> {
-		this.tryToInitialize();
+      this.producer.on('ready', function () {
+        this.ready = true;
+      });
 
-		return new Observable(observer => {
-			if (this.target) {
-				return this.target.produce(JSON.stringify(command), function(err,res){
-					if (err) {
-						observer.error(err);
-					}
-					else {
-						observer.next(res);
-						observer.complete();
-					}
-				});
-			}
-			else
-			{
-				observer.error("Not such a topic in  ")
-			}
-		});
-	}
+      this.producer.on('error', function (err) {
+        console.log('error', err);
+      });
+    }
+  }
 
+  sendCommand(...params: any[]): Observable<any> {
+    if (!this.ready) {
+      return Observable.throw("Please wait for connection...");
+    }
+
+    if (!params || params.length===0) {
+      return Observable.throw("No commands need to be sent.");
+    }
+
+    var subject = new Subject();
+    this.tryToInitialize();
+
+    let payload = [];
+    for (let i = 0; i < params.length; i++) {
+      payload.push({
+        topic: this.topicName,
+        partition: this.partitionId,
+        messages: [JSON.stringify(params[i])],
+        attributes: this.compressionAttributes
+      });
+    }
+
+    this.producer.send(payload, function (err, result) {
+      if (err) {
+        subject.error(err);
+      }
+      else {
+        subject.next(result);
+        subject.complete();
+      }
+      console.log(err || result);
+    });
+
+    return from(subject);
+  }
 }
 
 export class KafkaConsumer {
-	constructor(
-		public apiUrl: string,
-		public topicName: string,
-		public consumerGroup: string,
-		public fromBeginning: boolean
-	) { }
+  constructor(
+    public zookeeperConnectionString: string,
+    public topicName: string,
+    public consumerGroup: string,
+    public fromBeginning: boolean
+  ) { }
 
-	private kafka: any;
-	private target: any;
-	private consumerInstance: any;
-	private consumerConfig: any;
+  private client: Client;
+  private consumer: Consumer;
+  private clientoffset: Offset;
+  private consumerConfig: any;
 
-	private logShutdown(err, observer): void {
-		if (err && observer)
-			observer.error("Error while shutting down: " + err);
-		else
-			console.log("Shutdown cleanly.");
-	}
+  private logShutdown(err, observer): void {
+    if (err && observer)
+      observer.error("Error while shutting down: " + err);
+    else
+      console.log("Shutdown cleanly.");
+  }
 
-	consume(): Observable<string> {
-		return new Observable(observer => {
-			if (!this.kafka) {
-				this.consumerConfig = {
-					"format": "binary"
-				};
-				if (this.fromBeginning) {
-					this.consumerConfig['auto.offset.reset'] = 'smallest';
-				}
+  consume(): Observable<any> {
+    var subject = new Subject();
 
-				this.kafka = new KafkaRest({ 'url': this.apiUrl });
-				this.kafka.consumer(this.consumerGroup).join(this.consumerConfig, function (err, consumerInstance) {
-					if (err) {
-						observer.error(err);
-						return;
-					}
+    this.client = new Client(this.zookeeperConnectionString);
+    var topics = [{ topic: this.topicName, partition: 1 }, { topic: this.topicName, partition: 0 }];
+    var options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
 
-					this.consumerInstance = consumerInstance;
+    this.consumer = new Consumer(this.client, topics, options);
+    this.clientoffset = new Offset(this.client);
 
-					var stream = consumerInstance.subscribe(this.topicName);
-					stream.on('data', function (msgs) {
-						for (var i = 0; i < msgs.length; i++) {
-							observer.next(msgs[i].value.toString('utf8'));
-						}
-					}.bind(this));
+    this.consumer.on('message', function (message) {
+      console.log(message);
+      subject.next(message);
+    });
 
-					stream.on('error', function (err) {
-						observer.error(err);
-						this.consumerInstance.shutdown(this.logShutdown(err, observer));
-					}.bind(this));
-					stream.on('end', function () {
-					});
+    this.consumer.on('error', function (err) {
+      console.log('error', err);
+      subject.error(err);
+    });
 
-					this.consumerInstance.on('end', function () {
-						console.log("Consumer instance closed.");
-					});
-				}.bind(this));
-			}
-		});
-	}
+    /*
+    * If consumer get `offsetOutOfRange` event, fetch data from the smallest(oldest) offset
+    */
+    this.consumer.on('offsetOutOfRange', function (topic) {
+      topic.maxNum = 2;
+      this.offset.fetch([topic], function (err, offsets) {
+        if (err) {
+          subject.error(err);
+          return console.error(err);
+        }
+        var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+        this.consumer.setOffset(topic.topic, topic.partition, min);
+      });
+    });
 
-	stopConsumer(observer: any): void {
-		if (observer)
-			observer.complete();
+    return from(subject);
 
-		if (this.consumerInstance)
-			this.consumerInstance.shutdown(this.logShutdown);
-	}
-}
+  }
 
-
-export class KafkaManager {
-	private kafka: any;
-
-	constructor(
-		public apiUrl: string
-	) {
-		this.kafka = new KafkaRest({ 'url': apiUrl });
-	};
-
-
-	listBrokers(): Observable<any> {
-		return this.kafka.brokers.list()
-			.map(res => res.json())
-			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-	}
-	listTopics(): Observable<any> {
-		return this.kafka.topics.list()
-			.map(res => res.json())
-			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-	}
-
-	getSingleTopic(firstTopicName): Observable<any> {
-		if (firstTopicName === null) {
-			throw Observable.throw("Parameter firstTopicName is empty.");
-		}
-
-		return this.kafka.topic(firstTopicName).get()
-			.map(res => res.json())
-			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-	}
-	listTopicPartitions(firstTopicName): Observable<any> {
-		if (firstTopicName === null) {
-			throw Observable.throw("Parameter firstTopicName is empty.");
-		}
-
-		return this.kafka.topic(firstTopicName).partitions.list()
-			.map(res => res.json())
-			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-	}
-
-	getSingleTopicPartition(firstTopicName, firstTopicPartitionId) {
-		if (firstTopicName === null || firstTopicPartitionId === null) {
-			throw Observable.throw("Parameter firstTopicName or firstTopicPartitionId is empty.");
-		}
-
-		return this.kafka.topic(firstTopicName).partition(firstTopicPartitionId).get()
-			.map(res => res.json())
-			.catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-	}
-
+  stopConsumer(observer: any): void {
+    if (observer)
+      observer.complete();
+  }
 }
